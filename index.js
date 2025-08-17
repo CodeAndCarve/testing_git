af9e95ec536efaa59df3324b0d61d03f00e7b08d
CodeAndCarve <digvijays18@gmail.com>
2025-08-16 19:28:38 -0700

    fixes the bug

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("Hello, from anohter git practice")
 console.log("hello there github")
 console.log('this is so cool')
+//This is the bug fix
+function bugFix(){
+    console.log("boom bug fixed")
+}
